239854f40d88cffbe6dc5bc9597e0b1af755f82b
Spotlight <spotlight@joscomputing.space>
2020-03-23 13:59:26 -0500

    Update Socket to 1.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "0.12.78")
+    .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0")
   ],
   targets: [
     .target(
